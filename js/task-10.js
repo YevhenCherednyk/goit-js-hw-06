7ec5229848ef9c4d5f1fa29d81965fc9752cf60e
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-10-03 21:33:04 +0300

    added task-10

--- js/task-10.js
+++ js/task-10.js
@@ -16,6 +16,52 @@
 Використовуй готову функцію getRandomHexColor для отримання кольору.
 */
 
+const refs = {
+  inputNumberRef: document.querySelector('#controls input'),
+  createElementsBtnRef: document.querySelector('[data-create]'),
+  destroyElementsBtnRef: document.querySelector('[data-destroy]'),
+  divBoxesRef: document.querySelector('#boxes'),
+};
+
+refs.createElementsBtnRef.addEventListener('click', onCreateBtn);
+refs.destroyElementsBtnRef.addEventListener('click', onDestroyBtn);
+
+function onCreateBtn(event) {
+  createBoxes(refs.inputNumberRef.value);
+}
+
+function onDestroyBtn(event) {
+  refs.inputNumberRef.value = '';
+  refs.divBoxesRef.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+  const DIV_SIZE_INCREMENT = 10;
+  let divWidth = 30;
+  let divHeight = 30;
+  const divItemArr = [];
+
+  const itemAddRef = document.createElement('div');
+  refs.divBoxesRef.append(itemAddRef);
+
+  for (let i = 0; i < amount; i += 1) {
+    divWidth += DIV_SIZE_INCREMENT;
+    divHeight += DIV_SIZE_INCREMENT;
+    divItemArr.push(creatNewDiv(divWidth, divHeight));
+  }
+  refs.divBoxesRef.append(...divItemArr);
+}
+
+function creatNewDiv(divWidth, divHeight) {
+  const element = document.createElement('div');
+
+  element.style.width = `${divWidth}px`;
+  element.style.height = `${divHeight}px`;
+  element.style.backgroundColor = getRandomHexColor();
+
+  return element;
 }
